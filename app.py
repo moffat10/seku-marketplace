fdb70cb7cda8bd71a6ccbb156343581760d27b49
moffat okemwa <moffatokemwa5@gmail.com>
2024-02-09 23:02:11 +0300

    woi

--- app.py
+++ app.py
@@ -132,10 +132,9 @@ def addproduct():
 def advertise():
     return render_template('advert.html')
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
-if __name__=='__main__':    
+if __name__=='__main__':  
+    with app.app_context():
+        db.create_all()  
     app.run(host='0.0.0.0',debug=True)
