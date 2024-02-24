b5ead54b181d20b1566fd1461f563bcd54847d39
moffat okemwa <moffatokemwa5@gmail.com>
2024-02-24 16:19:32 +0300

    forgot password update

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail,Message
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import LoginManager,login_required,logout_user,login_user,UserMixin,current_user
 from werkzeug.utils import secure_filename
 import os
+import random
 
 app=Flask(__name__)
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY']='saystheking'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['UPLOAD_FOLDER']='static/files'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=465
+app.config['MAIL_USE_TLS']=False
+app.config['MAIL_USE_SSL']=True
+app.config['MAIL_USERNAME']='sekumarket@gmail.com'
+app.config['MAIL_PASSWORD']='gwwa wjls jekh bjgd'
+
+
+
 db=SQLAlchemy(app)
 login_manager=LoginManager()
 login_manager.init_app(app)
 login_manager.login_view="login"
+mail=Mail(app)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -22,6 +34,7 @@ def load_user(user_id):
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(50),unique=True,nullable=False)
+    email=db.Column(db.String(50),unique=True,nullable=False)
     location=db.Column(db.String(50),nullable=False)
     phone_number=db.Column(db.Integer,unique=True,nullable=False)
     password=db.Column(db.String(50),nullable=False)
@@ -42,7 +55,11 @@ class Products(db.Model,UserMixin):
     seller_name=db.Column(db.String(50),nullable=False)
     seller_contact=db.Column(db.String(50),nullable=False)
     seller_id=db.Column(db.Integer,db.ForeignKey('user.id'))
-    
+
+class Passrecovery(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(50),unique=True,nullable=False)
+    code=db.Column(db.String(50),unique=True,nullable=False)
 
 @app.route('/')
 def home():
@@ -57,11 +74,12 @@ def cat_return():
 def create_seller():
     if request.method=='POST':
         username=request.form['username']
+        email=request.form['email']
         location=request.form['location']
         phone_number=request.form['phone_number']
         password=request.form['password']
         hashed_password=generate_password_hash(password)
-        new_user=User(username=username,password=hashed_password,phone_number=phone_number,location=location)
+        new_user=User(username=username,email=email,password=hashed_password,phone_number=phone_number,location=location)
         existing_username=User.query.filter_by(username=username).first()
         existing_phone=User.query.filter_by(phone_number=phone_number).first()
         if existing_username:
@@ -85,11 +103,12 @@ def login():
         username=request.form['username']
         password=request.form['password']
         user=User.query.filter_by(username=username).first()
-
         if user:
            if check_password_hash(user.password,password):
                 login_user(user)
                 return redirect(url_for('dashboard')) 
+           else:
+               flash('Wrong username or password, please check and try again!')
         else:
             flash('Wrong username or password, please check and try again!','danger')
     return render_template('login.html')
@@ -144,6 +163,50 @@ def cat_view(cat):
 def proview(id):
     product_description=Products.query.filter_by(id=id)
     return render_template('product.html',product_description=product_description)
+@app.route('/reset',methods=['POST','GET'])
+def reset():
+    if request.method=='POST':
+        email=request.form['email']
+        user=User.query.filter_by(email=email).first()
+        if user:
+            code=random.randint(1000,9999)
+            new_recovery=Passrecovery(email=email,code=str(code))
+            db.session.add(new_recovery)
+            db.session.commit()
+            msg=Message(subject='Password Reset',sender='sekumarket@gmail.com',recipients=[email])
+            msg.body='Your code is '+str(code)
+            mail.send(msg)
+            flash('Reset code sent, Check your email')
+            return redirect(url_for('newpass'))
+    return render_template('resetpass.html')
+@app.route('/new_pass',methods=['POST','GET'])
+def newpass():
+    if request.method=='POST':
+        email=request.form['email']
+        code=request.form['code']
+        password=request.form['password']
+        passwordc=request.form['passwordc']
+        email_check=Passrecovery.query.filter_by(email=email).first()
+        if password==passwordc:
+            if email_check.code==code:
+                hashed_password=generate_password_hash(password)
+                user=User.query.filter_by(email=email).first()
+                user.password=hashed_password
+                db.session.commit()
+                flash('Password reset successfully')
+                if user.password==hashed_password:
+                    code_delete=Passrecovery.query.filter_by(email=email).first()
+                    db.session.delete(code_delete)
+                    db.session.commit()
+                    return redirect(url_for('login'))
+        else:
+            flash("Password did not match, try again !")
+    return render_template('newpass.html')
+    
+                
+                
+
+
 
 if __name__=='__main__':  
     with app.app_context():
