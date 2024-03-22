b2a78083c79c4d76ac294b66b4e5cc85a574d81a
moffat okemwa <moffatokemwa5@gmail.com>
2024-03-23 00:13:43 +0300

    march update

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import os
 import random
 
 app=Flask(__name__)
+
+#configure app and mail server
+
 app.config['SECRET_KEY']=os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 app.config['UPLOAD_FOLDER']='static/files'
@@ -19,18 +22,21 @@ app.config['MAIL_USERNAME']='sekumarket@gmail.com'
 app.config['MAIL_PASSWORD']=os.environ.get('PASSWORD')
 
 
+#create db object
 
 db=SQLAlchemy(app)
 login_manager=LoginManager()
 login_manager.init_app(app)
 login_manager.login_view="login"
+
+#create mail object
 mail=Mail(app)
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+#define user table
 class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(50),unique=True,nullable=False)
@@ -42,7 +48,7 @@ class User(db.Model,UserMixin):
     
     def is_active(self):
         return True
-    
+#create products table
 class Products(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(50),nullable=False)
@@ -51,25 +57,63 @@ class Products(db.Model,UserMixin):
     category=db.Column(db.String(20),nullable=False)
     product_nature=db.Column(db.String(50),nullable=False)
     price=db.Column(db.String(50),nullable=False)
-    price_terms=db.Column(db.String(50),nullable=False)
     seller_name=db.Column(db.String(50),nullable=False)
     seller_contact=db.Column(db.String(50),nullable=False)
     seller_id=db.Column(db.Integer,db.ForeignKey('user.id'))
 
+#create password recovery table
 class Passrecovery(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     email=db.Column(db.String(50),unique=True,nullable=False)
     code=db.Column(db.String(50),unique=True,nullable=False)
 
-@app.route('/')
+#define shopping kart table
+class Shoppingkart(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),nullable=False)
+    description=db.Column(db.String(100),nullable=False)
+    image_url=db.Column(db.String(50),nullable=False)
+    category=db.Column(db.String(20),nullable=False)
+    product_nature=db.Column(db.String(50),nullable=False)
+    price=db.Column(db.String(50),nullable=False)
+    seller_name=db.Column(db.String(50),nullable=False)
+    seller_contact=db.Column(db.String(50),nullable=False)
+    buyer_id=db.Column(db.Integer,nullable=False)
+
+#define sold items table
+class Solditems(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    order_number=db.Column(db.String(50),unique=True,nullable=False)
+    name=db.Column(db.String(50),nullable=False)
+    price=db.Column(db.String(50),nullable=False)
+    seller_name=db.Column(db.String(50),nullable=False)
+    seller_contact=db.Column(db.String(50),nullable=False)
+    buyer_id=db.Column(db.Integer,nullable=False)
+
+#chats table
+class Chatstable(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    message=db.Column(db.String(50),nullable=False)
+    buyer_id=db.Column(db.String(50),nullable=False)
+    seller_id=db.Column(db.String(50),nullable=False)
+    seller_name=db.Column(db.String(50),nullable=False)
+    buyer_name=db.Column(db.String(50),nullable=False)
+
+#home route
+@app.route('/',methods=['POST','GET'])
 def home():
     all_products=Products.query.limit(8).all()
+    if request.method=='POST':
+        flash('Login to buy!')
+        return redirect(url_for('login'))
     return render_template('home.html',all_products=all_products)
 
+#categories route
 @app.route('/categories')
 def cat_return():
     return render_template('categories.html')
 
+#create account route
 @app.route('/create-seller',methods=['GET','POST'])
 def create_seller():
     if request.method=='POST':
@@ -96,7 +140,7 @@ def create_seller():
     return render_template('create-seller.html')
 
 
-
+#login route
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method=='POST':
@@ -113,6 +157,7 @@ def login():
             flash('Wrong username or password, please check and try again!','danger')
     return render_template('login.html')
 
+#logout user
 @app.route('/logout')
 @login_required
 def logout():
@@ -120,12 +165,29 @@ def logout():
     flash('logged out')
     return redirect(url_for('login'))
 
+#user dashboard
 @app.route('/dashboard',methods=['GET','POST'])
 @login_required
 def dashboard():
-    user_products=Products.query.filter_by(seller_id=current_user.id)
-    return render_template('dashboard.html',user_products=user_products)
+    all_products=Products.query.all()
+    if request.method=='POST':
+        name=request.form['product-name']
+        image_url=request.form['product-image']
+        condition=request.form['product-condition']
+        price=request.form['product-price']
+        category=request.form['product-category']
+        seller_contact=request.form['seller-contact']
+        seller_name=request.form['seller-name']
+        description=request.form['product-description']
+        buyer_id=current_user.id
+        newcart=Shoppingkart(name=name,image_url=image_url,description=description,category=category,price=price,product_nature=condition,seller_contact=seller_contact,seller_name=seller_name,buyer_id=buyer_id)
+        db.session.add(newcart)
+        db.session.commit()
+        flash('Added to your cart, proceed to checkout!')
+        return redirect(url_for('dashboard'))
+    return render_template('dashboard.html',user_products=all_products)
 
+#add product
 @app.route('/addproduct',methods=['GET','POST'])
 @login_required
 def addproduct():
@@ -140,30 +202,34 @@ def addproduct():
         category=request.form['category']
         product_nature=request.form['product_nature']
         price=request.form['price']
-        price_terms=request.form['price_terms']
         seller_id=current_user.id
         seller_name=current_user.username
         seller_contact=current_user.phone_number
-        new_product=Products(name=name,description=description,image_url=image_url,category=category,product_nature=product_nature,price=price,price_terms=price_terms,seller_id=seller_id,seller_name=seller_name,seller_contact=seller_contact)
+        new_product=Products(name=name,description=description,image_url=image_url,category=category,product_nature=product_nature,price=price,seller_id=seller_id,seller_name=seller_name,seller_contact=seller_contact)
         db.session.add(new_product)
         db.session.commit()
         flash('Product Added successfully')
         return redirect(url_for('dashboard'))
     return render_template('addproduct.html')
 
+#advertise
 @app.route('/advertise')
 def advertise():
     return render_template('advert.html')
- 
+
+#filter by category
 @app.route('/category/<cat>',methods=['POST','GET'])
 def cat_view(cat):
     viewed=Products.query.filter_by(category=cat)
     return render_template('category_view.html',viewed=viewed)
 
+#view product details
 @app.route('/product/<id>')
 def proview(id):
     product_description=Products.query.filter_by(id=id)
     return render_template('product.html',product_description=product_description)
+
+#reset password
 @app.route('/reset',methods=['POST','GET'])
 def reset():
     if request.method=='POST':
@@ -180,6 +246,8 @@ def reset():
             flash('Reset code sent, Check your email')
             return redirect(url_for('newpass'))
     return render_template('resetpass.html')
+
+#update password
 @app.route('/new_pass',methods=['POST','GET'])
 def newpass():
     if request.method=='POST':
@@ -205,10 +273,105 @@ def newpass():
     return render_template('newpass.html')
     
                 
-                
+@app.route('/mycart',methods=['POST','GET'])
+@login_required
+def mycart():
+    usershopping=Shoppingkart.query.filter_by(buyer_id=current_user.id)
+    return render_template('cart.html',usershopping=usershopping)
 
+@app.route('/myproducts',methods=['POST','GET'])
+@login_required
+def myproducts():
+    myproduct=Products.query.filter_by(seller_contact=current_user.phone_number)
+    return render_template('myproducts.html',myproduct=myproduct)
 
+@app.route('/profile',methods=['POST','GET'])
+@login_required
+def profile():
+    return render_template('profile.html')
 
+@app.route('/history')
+@login_required
+def history():
+    return render_template('history.html')
+
+@app.route('/remove/<proid>')
+@login_required
+def remove(proid):
+    cart=Shoppingkart.query.filter_by(buyer_id=current_user.id,id=proid).first()
+    db.session.delete(cart)
+    db.session.commit()
+    return redirect(url_for('mycart'))
+
+@app.route('/delete/<proid>')
+@login_required
+def delete(proid):
+    prod=Products.query.filter_by(seller_id=current_user.id,id=proid).first()
+    db.session.delete(prod)
+    db.session.commit()
+    flash('Deleted!')
+    return redirect(url_for('dashboard'))
+
+@app.route('/deletemsg/<msgid>')
+@login_required
+def deletemsg(msgid):
+    del_message=Chatstable.query.filter_by(id=msgid).first()
+    db.session.delete(del_message)
+    db.session.commit()
+    flash('Deleted!')
+    return redirect(url_for('sent'))
+
+@app.route('/pay/<proid>')
+@login_required
+def pay(proid):
+    cart=Shoppingkart.query.filter_by(id=proid)
+    return render_template('pay.html',cart=cart)
+
+@app.route('/messages')
+@login_required
+def messages():
+    my_recieved_messages=Chatstable.query.filter_by(seller_id=current_user.id)
+    return render_template('messages.html',my_recieved_messages=my_recieved_messages)
+@app.route('/sent')
+@login_required
+def sent():
+    my_sent_messages=Chatstable.query.filter_by(buyer_id=current_user.id)
+    return render_template('sent.html',my_sent_messages=my_sent_messages)
+@app.route('/chat/<product>',methods=['POST','GET'])
+@login_required
+def chat(product): 
+    if request.method=="POST":
+        product_details=Products.query.filter_by(id=product).first()
+        message=request.form['message']
+        buyer_id=current_user.id
+        seller_id=product_details.seller_id
+        buyer_name=current_user.username
+        seller_name=product_details.seller_name
+        new_message=Chatstable(message=message,buyer_id=buyer_id,seller_id=seller_id,buyer_name=buyer_name,seller_name=seller_name)
+        db.session.add(new_message)
+        db.session.commit()
+        flash('Sent!')
+        return redirect(url_for('sent'))
+    return render_template('compose.html')
+
+@app.route('/replymsg/<id>',methods=['POST','GET'])
+@login_required
+def replymsg(id): 
+    if request.method=="POST":
+        reciever_details=Chatstable.query.filter_by(id=id).first()
+        message=request.form['message']
+        buyer_id=current_user.id
+        seller_id=reciever_details.buyer_id
+        buyer_name=current_user.username
+        seller_name=reciever_details.buyer_name
+        new_message=Chatstable(message=message,buyer_id=buyer_id,seller_id=seller_id,buyer_name=buyer_name,seller_name=seller_name)
+        db.session.add(new_message)
+        db.session.commit()
+        flash('Message sent successfully!')
+        return redirect(url_for('sent'))
+    return render_template('reply.html')
+
+#start app
 if __name__=='__main__':  
     with app.app_context():
         db.create_all()  
