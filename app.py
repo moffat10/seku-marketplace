5fd9650f31c4c6562f5aadfdf5ce601a920779f2
moffat okemwa <moffatokemwa5@gmail.com>
2024-03-27 00:34:52 +0300

    key9

--- app.py
+++ app.py
@@ -473,7 +473,7 @@ def lnmocallback():
 @app.route('/payment')
 @login_required
 def payment():
-    time.sleep('5')
+    time.sleep('10')
     code=s_code
     if code=='1032':
         flash('Transaction cancelled!')
